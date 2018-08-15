3b1c0431997ee286e5295b3df9ee38fc6f712776
UladzimirHerman <uladzimirherman@gmail.com>
2018-08-16 00:12:28 +0300

    pizza v2.5.5 - added logging and javadoc

--- project/pizza/src/main/java/com/gmail/herman/uladzimir/tag/CostDefinitionTag.java
+++ project/pizza/src/main/java/com/gmail/herman/uladzimir/tag/CostDefinitionTag.java
@@ -1,12 +1,24 @@
 package com.gmail.herman.uladzimir.tag;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 import java.io.IOException;
 import java.math.BigDecimal;
 
+/**
+ * Class {@link CostDefinitionTag} is used to determine
+ * the total cost of a particular product.
+ *
+ * @author Uladzimir Herman
+ */
 public class CostDefinitionTag extends BodyTagSupport {
+
+    private static final Logger LOGGER = LogManager.getLogger(CostDefinitionTag.class);
+
     private BigDecimal price;
     private int quantity;
 
@@ -18,14 +30,23 @@ public class CostDefinitionTag extends BodyTagSupport {
         this.quantity = quantity;
     }
 
+    /**
+     * Determination of the total cost of goods
+     *
+     * @return list of objects of a particular type
+     * @throws JspException exception of view level
+     */
     @Override
     public int doStartTag() throws JspException {
+
         try {
             JspWriter out = pageContext.getOut();
             out.write(String.valueOf(price.multiply(new BigDecimal(quantity))));
         } catch (IOException e) {
+            LOGGER.error("IOException occurred when writing the result: ", e);
             throw new JspException(e);
         }
+
         return SKIP_BODY;
     }
 
