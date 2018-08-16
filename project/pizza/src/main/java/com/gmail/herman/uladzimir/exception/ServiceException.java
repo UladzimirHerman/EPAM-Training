2ce68fe122d69f03d211aa82d10a84ddf27502a9
UladzimirHerman <uladzimirherman@gmail.com>
2018-08-16 16:17:53 +0300

    pizza v2.6.3 - added javadoc in exception package

--- project/pizza/src/main/java/com/gmail/herman/uladzimir/exception/ServiceException.java
+++ project/pizza/src/main/java/com/gmail/herman/uladzimir/exception/ServiceException.java
@@ -1,15 +1,37 @@
 package com.gmail.herman.uladzimir.exception;
 
+/**
+ * Class {@link ServiceException} is used to create exceptions, that occur
+ * at the service level. It's the heir of the {@link Exception}.
+ *
+ * @author Uladzimir Herman
+ */
 public class ServiceException extends Exception {
 
+    /**
+     * Constructs a new exception with the specified detail message
+     *
+     * @param message detail message
+     */
     public ServiceException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new exception with the specified cause
+     *
+     * @param cause specified cause
+     */
     public ServiceException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and cause
+     *
+     * @param message detail message
+     * @param cause   specified cause
+     */
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
     }
