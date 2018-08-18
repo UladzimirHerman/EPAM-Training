8d673ab963b888e09855a1f47c8ee8b825c1beff
UladzimirHerman <uladzimirherman@gmail.com>
2018-08-18 11:41:24 +0300

    pizza v2.7.2 - added javadoc in controller package

--- project/pizza/src/main/java/com/gmail/herman/uladzimir/controller/Controller.java
+++ project/pizza/src/main/java/com/gmail/herman/uladzimir/controller/Controller.java
@@ -2,8 +2,8 @@ package com.gmail.herman.uladzimir.controller;
 
 import com.gmail.herman.uladzimir.command.Command;
 import com.gmail.herman.uladzimir.command.CommandFactory;
-import com.gmail.herman.uladzimir.command.ResponseType;
-import com.gmail.herman.uladzimir.command.Route;
+import com.gmail.herman.uladzimir.route.ResponseType;
+import com.gmail.herman.uladzimir.route.Route;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +13,16 @@ import java.io.IOException;
 
 import static com.gmail.herman.uladzimir.command.AttributeName.COMMAND;
 
+/**
+ * Class {@link Controller} is the main controller of an application. It takes
+ * all requests and manages their processing.
+ *
+ * @author Uladzimir Herman
+ * @see RequestWrapper
+ * @see Command
+ * @see CommandFactory
+ * @see Route
+ */
 public class Controller extends HttpServlet {
 
     private static final String ENCODING = "UTF-8";
@@ -29,9 +39,18 @@ public class Controller extends HttpServlet {
         executeRequest(req, resp);
     }
 
+    /**
+     * Executing get and post methods
+     *
+     * @param req  request
+     * @param resp response
+     * @throws IOException      may occur when setting encoding or taking new page
+     * @throws ServletException may occur when executing forward
+     */
     private void executeRequest(HttpServletRequest req, HttpServletResponse resp)
             throws IOException, ServletException {
         req.setCharacterEncoding(ENCODING);
+
         RequestWrapper requestWrapper = new RequestWrapper();
         requestWrapper.initialize(req);
 
--- project/pizza/src/main/java/com/gmail/herman/uladzimir/controller/RequestWrapper.java
+++ project/pizza/src/main/java/com/gmail/herman/uladzimir/controller/RequestWrapper.java
@@ -7,30 +7,34 @@ import javax.servlet.http.Part;
 import java.io.IOException;
 import java.util.*;
 
+/**
+ * Class {@link RequestWrapper} is a wrapper for request. It gets and keeps
+ * needed attributes and parameters from controller.
+ *
+ * @author Uladzimir Herman
+ * @see Controller
+ */
 public class RequestWrapper {
 
+    private static final String EMPTY = "";
     private static final String REFERER = "referer";
     private static final String SKIPPED_START_ADDRESS = "http://localhost:8080";
 
-    private Map<String, String[]> requestParameters;
-    private Map<String, Object> requestAttributes = new HashMap<>();
     private Map<String, Object> sessionAttributes = new HashMap<>();
+    private Map<String, Object> requestAttributes = new HashMap<>();
+    private Map<String, String[]> requestParameters;
     private List<Part> parts;
     private String applicationPath;
     private String referer;
 
+    /**
+     * Getting necessary information and initializing data members
+     *
+     * @param request request from controller
+     */
     public void initialize(HttpServletRequest request) {
-        requestParameters = new HashMap<>(request.getParameterMap());
-
         Object object;
         String name;
-
-        Enumeration<String> requestAttributeNames = request.getAttributeNames();
-        while (requestAttributeNames.hasMoreElements()) {
-            name = requestAttributeNames.nextElement();
-            object = request.getAttribute(name);
-            requestAttributes.put(name, object);
-        }
 
         HttpSession session = request.getSession();
         Enumeration<String> sessionAttributeNames = session.getAttributeNames();
@@ -40,64 +44,137 @@ public class RequestWrapper {
             sessionAttributes.put(name, object);
         }
 
+        Enumeration<String> requestAttributeNames = request.getAttributeNames();
+        while (requestAttributeNames.hasMoreElements()) {
+            name = requestAttributeNames.nextElement();
+            object = request.getAttribute(name);
+            requestAttributes.put(name, object);
+        }
+
+        requestParameters = new HashMap<>(request.getParameterMap());
+
         try {
             parts = new ArrayList<>(request.getParts());
         } catch (IOException | ServletException e) {
             parts = null;
         }
 
-        applicationPath = request.getServletContext().getRealPath("");
+        applicationPath = request.getServletContext().getRealPath(EMPTY);
 
         referer = request.getHeader(REFERER);
     }
 
-    public String getRequestParameter(String key) {
-        return requestParameters.containsKey(key) ? requestParameters.get(key)[0] : "";
-    }
-
-    public void removeSessionAttribute(String key) {
-        sessionAttributes.put(key, null);
-    }
-
+    /**
+     * Adding session attribute by key
+     *
+     * @param key    key of attribute
+     * @param object object for putting
+     */
     public void putSessionAttribute(String key, Object object) {
         removeSessionAttribute(key);
         sessionAttributes.put(key, object);
     }
 
+    /**
+     * Getting session attribute by key
+     *
+     * @param key key of attribute
+     * @return particular object
+     */
     public Object getSessionAttribute(String key) {
         return sessionAttributes.get(key);
     }
 
+    /**
+     * Removing session attribute by key
+     *
+     * @param key key of attribute
+     */
+    public void removeSessionAttribute(String key) {
+        sessionAttributes.put(key, null);
+    }
+
+    /**
+     * Adding request attribute by key
+     *
+     * @param key    key of attribute
+     * @param object object for putting
+     */
     public void putRequestAttribute(String key, Object object) {
         removeRequestAttribute(key);
         requestAttributes.put(key, object);
     }
 
+    /**
+     * Getting request attribute by key
+     *
+     * @param key key of attribute
+     * @return particular object
+     */
     public Object getRequestAttribute(String key) {
         return requestAttributes.get(key);
     }
 
-    public void removeRequestAttribute(String key) {
+    /**
+     * Removing request attribute by key
+     *
+     * @param key key of attribute
+     */
+    private void removeRequestAttribute(String key) {
         requestAttributes.put(key, null);
     }
 
+    /**
+     * Getting request parameter by key
+     *
+     * @param key key for searching
+     * @return parameter if exists or empty string if parameter doesn't exist
+     */
+    public String getRequestParameter(String key) {
+        return requestParameters.containsKey(key) ?
+                requestParameters.get(key)[0] : EMPTY;
+    }
+
+    /**
+     * Updating request from wrapper
+     *
+     * @param request request from controller
+     */
     public void update(HttpServletRequest request) {
+
         for (Map.Entry<String, Object> attribute : requestAttributes.entrySet()) {
             request.setAttribute(attribute.getKey(), attribute.getValue());
         }
+
         for (Map.Entry<String, Object> attribute : sessionAttributes.entrySet()) {
             request.getSession().setAttribute(attribute.getKey(), attribute.getValue());
         }
+
     }
 
+    /**
+     * Getting the parts of the input file
+     *
+     * @return parts
+     */
     public List<Part> getParts() {
         return parts;
     }
 
+    /**
+     * Getting application path
+     *
+     * @return application path
+     */
     public String getApplicationPath() {
         return applicationPath;
     }
 
+    /**
+     * Getting URL of referrer
+     *
+     * @return particular URL
+     */
     public String getReferer() {
         return referer.substring(SKIPPED_START_ADDRESS.length());
     }
